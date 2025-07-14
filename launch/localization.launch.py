fb1368b0052ca1dbafb59cc21fbefd8c2c83330d
felipetrentin <felipetrentintecnico@hotmail.com>
2025-07-13 23:10:24 -0300

    remote control, SLAM and navigation working unoptimized

---
+++ launch/localization.launch.py
@@ -0,0 +1,53 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument, ExecuteProcess, SetEnvironmentVariable
+from launch.event_handlers import OnProcessExit
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+import launch_ros.descriptions
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # pkg_ros_gz_sim_demos = get_package_share_directory('ros_gz_sim_demos')
+    pkg_robotino_navigation = get_package_share_directory('robotino_navigation')
+    pkg_ros_slam_toolbox = get_package_share_directory('slam_toolbox')
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation time (true for simulation, false for real robot)'
+    )
+    
+    slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_slam_toolbox, 'launch', 'localization_launch.py')),
+        launch_arguments={
+            'slam_params_file': [os.path.join(pkg_robotino_navigation, 'config', 'localizer_params_online_async.yaml')],
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+        }.items(),
+    )
+
+    return LaunchDescription([
+        use_sim_time_arg,
+        slam_toolbox,
+    ])
---
+++ launch/teleop.launch.py
@@ -0,0 +1,45 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument, ExecuteProcess, SetEnvironmentVariable
+from launch.event_handlers import OnProcessExit
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+import launch_ros.descriptions
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # pkg_ros_gz_sim_demos = get_package_share_directory('ros_gz_sim_demos')
+    pkg_robotino_navigation = get_package_share_directory('robotino_navigation')
+    pkg_teleop = get_package_share_directory('teleop_twist_joy')
+    
+    teleop = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_teleop, 'launch', 'teleop-launch.py')),
+        launch_arguments={
+            'config_filepath': [os.path.join(pkg_robotino_navigation, 'config', 'ps4.yaml')],
+        }.items(),
+    )
+
+    return LaunchDescription([
+        teleop,
+    ])
